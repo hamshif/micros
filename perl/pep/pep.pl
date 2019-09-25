4d9c16bee2033b31e1f6764df38caa907915b0ee
Gideon <hamshif@gmail.com>
2019-09-25 18:28:48 +0300

    Demo Consume Kafka events one at a time Call script with Kafka payload value Wait for script to exit Listen to failure and STDOUT

--- perl/pep/pep.pl
+++ perl/pep/pep.pl
@@ -6,5 +6,32 @@
 use strict;
 use warnings;
 
-# Print a message.
-print "Hello, World!\n";
+my $W = "PERL_SCRIPT:    ";
+
+
+print "$W Hello, World!\n";
+
+my $total = $#ARGV + 1;
+my $counter = 1;
+
+# get script name
+my $scriptname = $0;
+
+print "$W Total args passed to $scriptname : $total\n";
+
+# Use loop to print all args stored in an array called @ARGV
+foreach my $a(@ARGV) {
+    print "$W Arg # $counter : $a\n";
+    $counter++;
+}
+
+# (2) we got two command line args, so assume they are the
+# first name and last name
+my $message=$ARGV[0];
+
+print "\n\n$W Message is, $message !!!\n\n";
+
+sleep(3);
+print STDOUT 'STDOUT:  garmushka';
+
+print STDERR 'STDERR:  fanatic';
